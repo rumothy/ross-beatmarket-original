897f018fbbdfd4e0df367797e7b3500a7818aeea
rumothy <rumothy@yahoo.com>
2020-03-12 13:20:23 -0400

    Partial update for auth.

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const keys = require("./constants/keys");
 const mongoose = require("mongoose");
 // const routes = require("./routes");
 
+const passport = require("passport");
+const router = express.Router();
+const db = require("./models");
+var isAuthenticated = require("./config/middleware/isAuthenticated");
+
 const path = require("path");
 const bodyParser = require("body-parser");
 
@@ -154,6 +159,67 @@ app.get("/api/producers/:id", producersController.findById);
 // mock User Routing
 app.get("/api/users/:id", usersController.findById);
 app.post("/api/users", usersController.create);
+
+// ------------------------------------------------------- end Routing
+
+router.post("/api/register", function(req, res) {
+  console.log("registering user");
+
+  //Do password validation here before attempting to register user, such as checking for password length, captial letters, special characters, etc.
+
+  db.User.register(
+    new db.User({ username: req.body.username, email: req.body.email }),
+    req.body.password,
+    function(err, user) {
+      if (err) {
+        console.log(err);
+        return res.json(err);
+      }
+      passport.authenticate("local")(req, res, function(data) {
+        res.json(req.user);
+      });
+    }
+  );
+});
+
+router.post("/api/login", function(req, res, next) {
+  passport.authenticate("local", function(err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.json(info);
+    }
+    req.logIn(user, function(err) {
+      if (err) {
+        return next(err);
+      }
+      return res.json(user);
+    });
+  })(req, res, next);
+});
+
+router.get("/api/logout", function(req, res) {
+  req.logout();
+  res.json({ message: "logged out" });
+});
+
+router.get("/api/user", function(req, res) {
+  console.log("available username");
+  if (req.query.username) {
+    db.User.find({ username: req.query.username })
+      .then(result => {
+        res.json({ length: result.length });
+      })
+      .catch(err => res.status(422).json(err));
+  } else {
+    res.json({ message: "no username entered for query" });
+  }
+});
+
+router.get("/api/authorized", isAuthenticated, function(req, res) {
+  res.json(req.user);
+});
 
 // ------------------------------------------------------- end Routing
 app.use(function(req, res) {
